3ddc5f90c76810b549664aa2d06a4c470f40713a
zhaoShuai <s74933347@gmail.com>
2025-12-06 13:16:25 +0800

    first commit

---
+++ src/main/java/com/uestc/sdcs/SdcsApplication.java
@@ -0,0 +1,13 @@
+package com.uestc.sdcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdcsApplication.class, args);
+    }
+
+}
